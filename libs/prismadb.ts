685c59767dd012c8031787640d52048078320646
Gongming <gm.shi@outlook.com>
2023-07-31 18:35:33 +1000

    prisma

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -5,6 +5,6 @@ declare global {
 }
 
 const prisma = global.prisma || new PrismaClient({ log: ["info"] });
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+global.prisma = prisma;
 
 export default prisma;
